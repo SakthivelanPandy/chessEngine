4ec8daa0049a0d77a834c60a6f91a6a90b28e032
SakthivelanPandy <81441328+SakthivelanPandy@users.noreply.github.com>
2022-11-11 17:57:38 +0000

    Validate wether input follows LAN

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from Board import Board
+import re
 
-def check_input(user_input):
-  return 
+def validate_format(user_input):
+    lan = r"([KQNBR]?)([a-g][1-9])([x]?)([a-g][1-9])([KQNBR]?)"
+    if re.search(lan, user_input):
+       return re.split(lan, user_input)[1:-1]
 
 def main():
   game_board = Board()
   print(game_board.create_board_string())
+  print(validate_format("Bb4xc5"))
 
 
 if __name__ == '__main__':
