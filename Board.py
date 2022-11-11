ba94d3ee5b64722abbdfd3a8f12709a3fac50e6f
Alan Kraszewski <113364668+krasz47@users.noreply.github.com>
2022-11-11 12:03:48 +0000

    Add unicode charsets for pieces. Changed some tabs to spaces.

--- Board.py
+++ Board.py
@@ -1,15 +1,29 @@
 class Board:
     def __init__(self):
-        pieces = ["r","n","b","q","k","b","n","r"]
+        w_charset = {'k':'\u2654',                                              # Unicode charsets so that the board can be printed in the terminal with cool characters
+                     'q':'\u2655',
+                     'r':'\u2656',
+                     'b':'\u2657',
+                     'n':'\u2658',
+                     'p':'\u2659'}
+        b_charset = {'k':'\u265a',
+                     'q':'\u265b',
+                     'r':'\u265c',
+                     'b':'\u265d',
+                     'n':'\u265e',
+                     'p':'\u265f'}
+        
+        pieceArrangement = ["r","n","b","q","k","b","n","r"]
         pawns = ["p" for p in range(8)]
         blank = ["." for i in range(8)]
         self.board_state = []
-        self.board_state.append(pieces)
-        self.board_state.append(pawns)
+        self.board_state.append([b_charset[piece] for piece in pieceArrangement])
+        self.board_state.append([b_charset[pawn] for pawn in pawns])
         for i in range(4):
             self.board_state.append(blank)
-        self.board_state.append([pawn.upper() for pawn in pawns])
-        self.board_state.append([piece.upper() for piece in pieces])
+        self.board_state.append([w_charset[pawn] for pawn in pawns])                #Set before the piece arrangement so that the pawns are in the correct order
+        self.board_state.append([w_charset[piece] for piece in pieceArrangement])
+        
 		
     def create_board_string(self, flipped=False):
         output = ''
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from Board import Board
 
 def check_input(user_input):
-	
   return 
 
 def main():
-	game_board = Board()
-	print(game_board.create_board_string(flipped=True))
-
+  game_board = Board()
+  print(game_board.create_board_string())
 
 
 if __name__ == '__main__':
